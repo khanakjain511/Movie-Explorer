8de5f5cc63f52b6aa3c7dce2a7b7538f940f30fc
khanakjain511 <khanakjain511@gmail.com>
2026-04-01 23:10:04 +0530

    Added JS, CSS and updated HTML for Milestone 2

---
+++ script.js
@@ -0,0 +1,34 @@
+const API_KEY = "your_api_key_here";
+
+function searchMovie() {
+  let query = document.getElementById("search").value;
+  let moviesDiv = document.getElementById("movies");
+
+  moviesDiv.innerHTML = "Loading...";
+
+  fetch(`https://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`)
+    .then(res => res.json())
+    .then(data => {
+      moviesDiv.innerHTML = "";
+
+      if (data.Response === "True") {
+        data.Search.map(movie => {
+          let div = document.createElement("div");
+          div.className = "movie";
+
+          div.innerHTML = `
+            <img src="${movie.Poster}" />
+            <h3>${movie.Title}</h3>
+            <p>${movie.Year}</p>
+          `;
+
+          moviesDiv.appendChild(div);
+        });
+      } else {
+        moviesDiv.innerHTML = "No movies found";
+      }
+    })
+    .catch(() => {
+      moviesDiv.innerHTML = "Error loading data";
+    });
+}
